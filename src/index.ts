bf2c23332480632874cce07f6d84886151120401
HakeemDAbdulmalik <hakeem.d.abdulmalik@gmail.com>
2023-08-14 10:49:32 -0400

    Use http to download file

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,17 @@
-// const { chromium } = require('playwright');
-import { chromium } from "playwright";
+import fs from 'fs';
+import https from 'https'
 
+// URL of the image
+const url = 'https://xis.fifteennet.com/_v10/737468cdc60a4530fc37b79ac3b0b8d69498c6f1d5fa8661f6ea23c0536ab100fe7d8817349b40ab4354e46046a8ca7944d21d1227f702d8ca83d748f9323f1cb9f5872826ef5d7236885d88b78c50e487ecbbb67ee291554b8459c6a7adf6ee9d5c429089f861d9d3bd2c0aaed5f7dd38643461accfd1c0b9abde56c65bc35fe08bfb9f1a9d5d81a1079333998034d6/1080/index.m3u8';
 
-
-(async () => {
-  // Launch a new browser instance
-  const browser = await chromium.launch({ headless: true });
-
-  const url = 'https://webbrowsertools.com/test-download-with/';
-
-  // Create a new page
-  const page = await browser.newPage();
-
-    console.log(url);
-
-    await page.goto(url);
-
-    // Start waiting for download before clicking. Note no await.
-    const downloadPromise = page.waitForEvent('download');
-    await page.locator('#downloadButton').click();
-    const download = await downloadPromise;
-    // Wait for the download process to complete
-    // console.log(await download.path());
-    // Save downloaded file somewhere
-    await download.saveAs('./download/test.png');
-
-    // Navigate to the URL where the file download is triggered
-    // page.on('download', download => download.path().then(console.log));
-
-    // Click on the link or button that initiates the download
-    // await page.click('selector-for-download-link-or-button');
-
-    // Wait for the download to complete (you might need to adjust the wait time)
-    await page.waitForTimeout(10000); // Wait for 5 seconds (adjust as needed)
-
-
-
-  // Find the routes on the page to download files
-  // Try and use this for streaming video data.
-  // await page.route('**/*', route => {
-  //   route.
-  //   if (route.response().url().endsWith('.pdf')) {
-  //     const downloadFilePath = './download/test.pdf'; // Specify the desired file path
-  //     const buffer = route.response().body();
-  //     fs.writeFileSync(downloadFilePath, buffer);
-  //     route.abort();
-  //   } else {
-  //     route.continue();
-  //   }
-  // });
-
-  // Close the browser
-  await browser.close();
-})();
+https.get(url,(res) => {
+	// Image will be stored at this path
+	const path = `./downloads/`;
+  const fileName = 'index.m3u8';
+	const filePath = fs.createWriteStream(path+fileName);
+	res.pipe(filePath);
+	filePath.on('finish',() => {
+		filePath.close();
+		console.log('Download Completed');
+	})
+})
