52a767f7ec8be9bbbdb0933029f114786638450b
HakeemDAbdulmalik <hakeem.d.abdulmalik@gmail.com>
2023-08-14 10:18:53 -0400

    Save data using playwright

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,38 @@
 import { chromium } from "playwright";
 
 
-const movieUrl = 'http://demo.borland.com/testsite/download_testpage.php';
-
 
 (async () => {
   // Launch a new browser instance
   const browser = await chromium.launch({ headless: true });
 
+  const url = 'https://webbrowsertools.com/test-download-with/';
+
   // Create a new page
   const page = await browser.newPage();
-  await page.goto(movieUrl);
+
+    console.log(url);
+
+    await page.goto(url);
+
+    // Start waiting for download before clicking. Note no await.
+    const downloadPromise = page.waitForEvent('download');
+    await page.locator('#downloadButton').click();
+    const download = await downloadPromise;
+    // Wait for the download process to complete
+    // console.log(await download.path());
+    // Save downloaded file somewhere
+    await download.saveAs('./download/test.png');
+
+    // Navigate to the URL where the file download is triggered
+    // page.on('download', download => download.path().then(console.log));
+
+    // Click on the link or button that initiates the download
+    // await page.click('selector-for-download-link-or-button');
+
+    // Wait for the download to complete (you might need to adjust the wait time)
+    await page.waitForTimeout(10000); // Wait for 5 seconds (adjust as needed)
+
 
 
   // Find the routes on the page to download files
@@ -27,24 +49,6 @@ const movieUrl = 'http://demo.borland.com/testsite/download_testpage.php';
   //     route.continue();
   //   }
   // });
-
-  // Start waiting for download before clicking. Note no await.
-const downloadPromise = page.waitForEvent('download');
-await page.locator('#downloadButton').click();
-const download = await downloadPromise;
-// Wait for the download process to complete
-// console.log(await download.path());
-// Save downloaded file somewhere
-await download.saveAs('./download/test.pdf');
-
-  // Navigate to the URL where the file download is triggered
-  // page.on('download', download => download.path().then(console.log));
-
-  // Click on the link or button that initiates the download
-  // await page.click('selector-for-download-link-or-button');
-
-  // Wait for the download to complete (you might need to adjust the wait time)
-  await page.waitForTimeout(10000); // Wait for 5 seconds (adjust as needed)
 
   // Close the browser
   await browser.close();
