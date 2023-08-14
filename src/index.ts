2ef9da402baa84f5c1b7af2f65b0ae6123456bef
HakeemDAbdulmalik <hakeem.d.abdulmalik@gmail.com>
2023-08-14 18:13:19 -0400

    Save files directly to s3 from https call

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,75 @@
-import fs from 'fs';
-import https from 'https'
+// import fs from 'fs';
+import https from 'https';
+import { Readable } from 'stream';
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import dotenv from 'dotenv';
+import http from 'http';
 
-const client = new S3Client({});
+dotenv.config();
 
-export const main = async () => {
-  const command = new PutObjectCommand({
-    Bucket: "test-bucket",
-    Key: "hello-s3.txt",
-    Body: "Hello S3!",
+const s3Client = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+  },
+});
+
+async function uploadStreamToS3(readableStream: any, bucketName: string, objectKey: string) {
+  const uploadCommand = new PutObjectCommand({
+    Bucket: bucketName,
+    Key: objectKey,
+    Body: readableStream,
   });
 
   try {
-    const response = await client.send(command);
-    console.log(response);
+    await s3Client.send(uploadCommand);
+    console.log("Upload successful");
   } catch (err) {
-    console.error(err);
+    console.error("Error uploading:", err);
   }
-};
+}
+
+async function main() {
+  const bucketName: any = process.env.BUCKET;
+  const objectKey = 'seg-1-v1-a1.jpg';
+  const url = "https://xis.fifteennet.com/_v10/737468cdc60a4530fc37b79ac3b0b8d69498c6f1d5fa8661f6ea23c0536ab100fe7d8817349b40ab4354e46046a8ca7944d21d1227f702d8ca83d748f9323f1cb9f5872826ef5d7236885d88b78c50e487ecbbb67ee291554b8459c6a7adf6ee9d5c429089f861d9d3bd2c0aaed5f7dd38643461accfd1c0b9abde56c65bc35fe08bfb9f1a9d5d81a1079333998034d6/1080/seg-1-v1-a1.jpg"; // Replace with the desired URL
+
+  const response = await new Promise<http.IncomingMessage>((resolve, reject) => {
+    const request = https.get(url, resolve);
+    request.on("error", reject);
+  });
+
+  if (response.statusCode === 200) {
+    const readableStream = response.read();
+    // const readableStream = response;
+
+    uploadStreamToS3(readableStream, bucketName, objectKey);
+  } else {
+    console.error("Error downloading: Status Code", response.statusCode);
+  }
+}
+
+main();
+
+
+
 
 // URL of the image
-const url = 'https://xis.fifteennet.com/_v10/737468cdc60a4530fc37b79ac3b0b8d69498c6f1d5fa8661f6ea23c0536ab100fe7d8817349b40ab4354e46046a8ca7944d21d1227f702d8ca83d748f9323f1cb9f5872826ef5d7236885d88b78c50e487ecbbb67ee291554b8459c6a7adf6ee9d5c429089f861d9d3bd2c0aaed5f7dd38643461accfd1c0b9abde56c65bc35fe08bfb9f1a9d5d81a1079333998034d6/1080/index.m3u8';
+// const url = 'https://xis.fifteennet.com/_v10/737468cdc60a4530fc37b79ac3b0b8d69498c6f1d5fa8661f6ea23c0536ab100fe7d8817349b40ab4354e46046a8ca7944d21d1227f702d8ca83d748f9323f1cb9f5872826ef5d7236885d88b78c50e487ecbbb67ee291554b8459c6a7adf6ee9d5c429089f861d9d3bd2c0aaed5f7dd38643461accfd1c0b9abde56c65bc35fe08bfb9f1a9d5d81a1079333998034d6/1080/index.m3u8';
 
-const path = `./downloads/`;
-const fileName = 'index.m3u8';
+// const path = `downloads/`;
+// const fileName = 'index.m3u8';
 
-// const url = 'https://xis.fifteennet.com/_v10/737468cdc60a4530fc37b79ac3b0b8d69498c6f1d5fa8661f6ea23c0536ab100fe7d8817349b40ab4354e46046a8ca7944d21d1227f702d8ca83d748f9323f1cb9f5872826ef5d7236885d88b78c50e487ecbbb67ee291554b8459c6a7adf6ee9d5c429089f861d9d3bd2c0aaed5f7dd38643461accfd1c0b9abde56c65bc35fe08bfb9f1a9d5d81a1079333998034d6/1080/seg-4-v1-a1.html'
-
-https.get(url,(res) => {
+// https.get(url,(res) => {
 
 
-	const filePath = fs.createWriteStream(path+fileName);
-	res.pipe(filePath);
-	filePath.on('finish',() => {
-		filePath.close();
-		console.log('Download Completed');
-	})
-});
+// 	const filePath = fs.createWriteStream(path+fileName);
+// 	res.pipe(filePath);
+// 	filePath.on('finish',() => {
+// 		filePath.close();
+// 		console.log('Download Completed');
+// 	})
+// });
 
-// open file and get each line
-
-// Asynchronously read the file
-
-const filePath = path+fileName;
-fs.readFile(filePath, 'utf-8', (err, data) => {
-  if (err) {
-    console.error('Error reading file:', err);
-    return;
-  }
-
-  // Split the file content into lines
-  const lines = data.split('\n');
-  const newUrl = url.replace(fileName, '');
-
-  // Loop through each line and do something with it
-  for (const line of lines) {
-    if (line[0] !== '#'){
-
-      https.get(newUrl+line,(res) => {
-
-        const filePath = fs.createWriteStream(path+line);
-        res.pipe(filePath);
-        filePath.on('finish',() => {
-          filePath.close();
-          console.log('Download Completed! File name:', line);
-        })
-      });
-
-    }
-  }
-});
 
