4764f5c4e0f63cf8ecedc249e165092523bc046f
HakeemDAbdulmalik <hakeem.d.abdulmalik@gmail.com>
2023-08-15 09:59:59 -0400

    Save data directly to the S3 bucket.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-// import fs from 'fs';
-import https from 'https';
 import { Readable } from 'stream';
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import dotenv from 'dotenv';
-import http from 'http';
+import axios from 'axios';
 
 dotenv.config();
 
@@ -15,7 +13,11 @@ const s3Client = new S3Client({
   },
 });
 
-async function uploadStreamToS3(readableStream: any, bucketName: string, objectKey: string) {
+// Is this accepting the stream
+async function uploadStreamToS3(readableStream: Readable, bucketName: string|undefined, objectKey: string) {
+
+  // readableStream.pipe()
+
   const uploadCommand = new PutObjectCommand({
     Bucket: bucketName,
     Key: objectKey,
@@ -24,6 +26,7 @@ async function uploadStreamToS3(readableStream: any, bucketName: string, objectK
 
   try {
     await s3Client.send(uploadCommand);
+    console.log('Add the ability to edit the data with a transform call');
     console.log("Upload successful");
   } catch (err) {
     console.error("Error uploading:", err);
@@ -31,45 +34,22 @@ async function uploadStreamToS3(readableStream: any, bucketName: string, objectK
 }
 
 async function main() {
-  const bucketName: any = process.env.BUCKET;
+  const bucketName: string|undefined = process.env.BUCKET;
   const objectKey = 'index.m3u8';
-  const url = "https://xis.fifteennet.com/_v10/737468cdc60a4530fc37b79ac3b0b8d69498c6f1d5fa8661f6ea23c0536ab100fe7d8817349b40ab4354e46046a8ca7944d21d1227f702d8ca83d748f9323f1cb9f5872826ef5d7236885d88b78c50e487ecbbb67ee291554b8459c6a7adf6ee9d5c429089f861d9d3bd2c0aaed5f7dd38643461accfd1c0b9abde56c65bc35fe08bfb9f1a9d5d81a1079333998034d6/1080/index.m3u8"; // Replace with the desired URL
+  const url = "https://xis.fifteennet.com/_v10/737468cdc60a4530fc37b79ac3b0b8d69498c6f1d5fa8661f6ea23c0536ab100fe7d8817349b40ab4354e46046a8ca7944d21d1227f702d8ca83d748f9323f1cb9f5872826ef5d7236885d88b78c50e487ecbbb67ee291554b8459c6a7adf6ee9d5c429089f861d9d3bd2c0aaed5f7dd38643461accfd1c0b9abde56c65bc35fe08bfb9f1a9d5d81a1079333998034d6/1080/index.m3u8";
+  try {
+    const response = await axios.get(url);
 
-  const response = await new Promise<http.IncomingMessage>((resolve, reject) => {
-    const request = https.get(url, resolve);
-    request.on("error", reject);
-  });
+    if (response.status === 200) {
+      const readableStream = response.data;
 
-  if (response.statusCode === 200) {
-    const readableStream = response.read();
-    // const readableStream = response;
-
-    uploadStreamToS3(readableStream, bucketName, objectKey);
-  } else {
-    console.error("Error downloading: Status Code", response.statusCode);
+      uploadStreamToS3(readableStream, bucketName, objectKey);
+    } else {
+      console.error("Error downloading: Status Code", response.status);
+    }
+  } catch (error: any) {
+    console.error("Error downloading:", error.message);
   }
 }
 
 main();
-
-
-
-
-// URL of the image
-// const url = 'https://xis.fifteennet.com/_v10/737468cdc60a4530fc37b79ac3b0b8d69498c6f1d5fa8661f6ea23c0536ab100fe7d8817349b40ab4354e46046a8ca7944d21d1227f702d8ca83d748f9323f1cb9f5872826ef5d7236885d88b78c50e487ecbbb67ee291554b8459c6a7adf6ee9d5c429089f861d9d3bd2c0aaed5f7dd38643461accfd1c0b9abde56c65bc35fe08bfb9f1a9d5d81a1079333998034d6/1080/index.m3u8';
-
-// const path = `downloads/`;
-// const fileName = 'index.m3u8';
-
-// https.get(url,(res) => {
-
-
-// 	const filePath = fs.createWriteStream(path+fileName);
-// 	res.pipe(filePath);
-// 	filePath.on('finish',() => {
-// 		filePath.close();
-// 		console.log('Download Completed');
-// 	})
-// });
-
-
